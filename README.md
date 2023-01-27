a0834b812c43fa06c00ce1ea725cb73a14502f05
damian.rzepiszczak <damian.rzepiszczak@gmail.com>
2023-01-27 16:21:55 +0100

    Add API schema generation

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Simple REST HTTP API service for consuming details about users from github repository.
 
 Technology used: Java 11, Spring Boot, H2
+
+Schema is generated after service startup under localhost:8080/swagger-ui.html. It uses OpenAPI3.0 specification and 
+swagger interface.
--- src/main/java/rzepi/dam/users/UserDetailsServiceApplication.java
+++ src/main/java/rzepi/dam/users/UserDetailsServiceApplication.java
@@ -1,9 +1,12 @@
 package rzepi.dam.users;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title="User details service API"))
 public class UserDetailsServiceApplication {
 
 	public static void main(String[] args) {
